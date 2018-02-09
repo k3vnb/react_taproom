9ed54b8aee09154aff14d6eff50c76d8a0c0e4d4
Kevin Boyle <papershack@gmail.com>
2018-02-09 14:43:39 -0800

    set up error404 page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Nav from './Nav';
 import Footer from './Footer';
 import Edit from './Edit';
 import BeerList from './BeerList';
+import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
 function App(){
@@ -13,6 +14,7 @@ function App(){
         <Route exact path='/' component={BeerList} />
         <Route path='/edit' component={Edit} />
         <Route path='/addnew' component={Edit} />
+        <Route component={Error404} />
       </Switch>
       <Footer />
     </div>
---
+++ src/components/Error404.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import confused from './../confused.jpg';
+import { Link } from 'react-router-dom';
+
+const error404 = {
+  padding: '5%',
+  marginLeft: 100
+}
+const confusedImg = {
+  borderRadius: '50%',
+  height: '30vh',
+  width: '20vw',
+  boxShadow: '1px 1px 1px black',
+  marginLeft: 50
+}
+
+function Error404(){
+  return(
+    <div style={error404}>
+      <img style={confusedImg} src={confused}/>
+      <h1>error 404</h1>
+      <h2>Oops! You're not supposed to be here</h2>
+      <h3>Go back to the <Link to='/'>homepage</Link></h3>
+    </div>
+  );
+}
+
+export default Error404;
