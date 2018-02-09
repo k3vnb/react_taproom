d1fd1fa3ea44f70d7c73beca626da3e167d8b21e
Kevin Boyle <papershack@gmail.com>
2018-02-09 13:35:25 -0800

    set up subcomponents of Edit component, with if statement evaluating href of url path to determine view

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -3,11 +3,25 @@ import { Switch, Router } from 'react-router-dom';
 
 
 function Edit() {
-  return(
-    <div>
-      Edit Works
-    </div>
-  );
+  if (window.location.href.indexOf('edit') > -1){
+    return(
+      <div>
+        <div>
+          Edit Beer
+        </div>
+      </div>
+    );
+
+  } else if (window.location.href.indexOf('addnew') > -1){
+    return(
+      <div>
+        <div>
+          Add New Beer
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default Edit;
