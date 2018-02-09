4bbf609d2af18a43a707a1e21152c4e47edac7a6
Kevin Boyle <papershack@gmail.com>
2018-02-09 13:11:56 -0800

    set up router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import Edit from './Edit';
 import BeerList from './BeerList';
+import { Switch, Router } from 'react-router-dom';
 
 function App(){
   return (
     <div>
       <Nav />
       <BeerList />
+      <Edit />
       <Footer />
     </div>
   );
---
+++ src/components/Edit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Router } from 'react-router-dom';
+
+
+function Edit() {
+  return(
+    <div>
+      Edit Works
+    </div>
+  );
+}
+
+export default Edit;
