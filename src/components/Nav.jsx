0f33262fb0cff9360658a01959f431ff20073a5a
Kevin Boyle <papershack@gmail.com>
2018-02-09 11:18:19 -0800

    create draft navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const navFlex = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+}
+
+function Nav() {
+  return (
+    <div style={navFlex}>
+      <div>
+        <h1>React.Taps</h1>
+      </div>
+      <div>
+        <ul style={navFlex}>
+          <li><a>Edit Beers</a></li>
+          <li><a>Add New Beers</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Nav
